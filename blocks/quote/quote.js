dbdd1e9101554241146822130357b8d85c66183d
shoyabsyed4646 <sshoyab@wearebarbarian.com>
2025-09-18 14:23:37 +0530

    quote html refactoring update

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,25 @@
+import { moveInstrumentation } from "../../scripts/scripts";
+
 export default function decorate(block) {
-    const [quoteWrapper] = block.children;
-  
-    const blockquote = document.createElement('blockquote');
-    blockquote.textContent = quoteWrapper.textContent.trim();
-    quoteWrapper.replaceChildren(blockquote);
-  }
+  // Extract field wrappers: [quoteDiv, authorDiv]
+  const [quoteWrapper, authorWrapper] = block.children;
+
+  // --- QUOTE ---
+  const blockquote = document.createElement('blockquote');
+  blockquote.innerHTML = quoteWrapper.innerHTML.trim();
+
+  // Move AEM instrumentation attributes (data-aue-prop) to <blockquote>
+  moveInstrumentation(quoteWrapper, blockquote);
+
+
+  // --- AUTHOR ---
+  const cite = document.createElement('cite');
+  cite.innerHTML = authorWrapper.textContent.trim();
+
+  // Move AEM instrumentation attributes to <cite>
+  moveInstrumentation(authorWrapper, cite);
+
+
+  // --- Replace block contents with semantic HTML ---
+  block.replaceChildren(blockquote, cite);
+}
